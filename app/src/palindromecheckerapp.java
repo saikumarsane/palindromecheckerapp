7fbda240fbe28afd1165e1ec136afdaee7227bac
saikumar <workinsaikumar>
2026-03-06 09:09:53 +0530

    uc1 commited

---
+++ app/src/palindromecheckerapp.java
@@ -0,0 +1,13 @@
+public class palindromecheckerapp {
+
+        public static void main(String[] args){
+            System.out.println("Welcome to palindrome check app");
+            System.out.println("version : 1.0");
+            System.out.println("system initialized successfully ");
+
+
+
+
+
+    }
+}
